d07b88f623f5a6999b70f8239028aeaf8090b4d2
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-10-25 22:32:44 +0530

    added summary

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -5,12 +5,12 @@ const Resume = () => {
     const { bio } = GlobalStates()
 
     return (
-        <div id="Resume">
-            <div className="p-4 flex flex-col items-center justify-center">
+        <div id="Resume" className="m-14">
+            <div className="">
                 <h1 className="font-medium text-3xl">{bio.candidateName}</h1>
                 <h3>{bio.currentStatus}</h3>
             </div>
-            <div className="flex items-center justify-around">
+            <div className="">
                 <div>
                     <h4>{bio.email}</h4>
                     <h4>{bio.number}</h4>
@@ -19,6 +19,10 @@ const Resume = () => {
                     <h4>{bio.address}</h4>
                     <h4>{bio.website}</h4>
                 </div>
+            </div>
+            <div className="">
+                <h2 className="text-xl font-medium">Summary</h2>
+                <p className="">{bio.summary}</p> 
             </div>
         </div>
     )
